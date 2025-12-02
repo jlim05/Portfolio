f01b6d0ed7b32f377b72219fd2b3974bb4268fcd
Jason <jasonlimnzz@gmail.com>
2025-12-02 19:37:34 +1300

    Navbar initial setup

---
+++ my-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <header className="navbar">
+      <div className="logo">Jason Lim</div>
+      <nav>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#skills">Skills</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
